0b3175111b9864e9190c3850ef50e805cd545850
yongji.yang <yongji.yang@kcl.ac.uk>
2022-09-29 21:54:17 +0100

    Find the first given amount of primes starting from 2

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    prime = 2
+
+    while len(list) < number_of_primes:
+        list.append(prime)
+        num = prime + 1
+        while not isPrime(num):
+            num += 1
+        prime = num
+
     return list
+
+
+def isPrime(number):
+    counter = 2
+    while counter < number:
+        if number % counter == 0:
+            return False
+        counter += 1
+    return True
